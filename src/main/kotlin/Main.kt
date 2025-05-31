026318d38d98fa3d6dfd87136d419d440c372064
Max Komarychev <maxkomarychev@gmail.com>
2025-05-31 23:12:36 +0200

    Primitive dialog

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,10 +6,22 @@ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     val apiKey = System.getenv("GEMINI_API_KEY")
-
     val modelId = "gemini-2.0-flash"
     val client = Client.builder().apiKey(apiKey).httpOptions(HttpOptions.builder().apiVersion("v1").build()).build()
-    val userInput = readLine()
-    val response = client.models.generateContent(modelId, userInput, null)
-    println(response)
+//    val chat = client.chats.create()
+    val systemPrompt = """
+        You are a chat bot. My questions will start with ">" and your answers start with "<".
+    """.trimIndent()
+    var history = systemPrompt
+    while (true) {
+        val userInput = readLine()
+        if (userInput == "exit") {
+            break
+        }
+        history = history + "\n" + userInput
+        val response = client.models.generateContent(modelId, history, null)
+        val text = response.text()
+        history = history + "\n" + text
+        println(text)
+    }
 }

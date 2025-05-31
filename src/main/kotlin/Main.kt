82747ae9d4ad02a562fdec639f41bfc9b7e8c57f
Max Komarychev <maxkomarychev@gmail.com>
2025-05-31 23:50:36 +0200

    ls

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,32 @@
 package com.aiagent
 
 import com.google.genai.Client
-import com.google.genai.types.HttpOptions
+import com.google.genai.types.*
 import kotlinx.coroutines.runBlocking
+
+val lsDeclaration =
+    FunctionDeclaration.builder().name("ls").description("List files in a specified directory")
+        .parameters(
+            Schema.builder().type(Type.Known.OBJECT).properties(
+                mapOf("path" to Schema.builder().type(Type.Known.STRING).description("Path to the directory").build())
+            ).build()
+        )
+        .response(
+            Schema.builder().type(Type.Known.OBJECT).properties(
+                mapOf(
+                    "output" to Schema.builder().type(Type.Known.STRING).description("Output of the ls command").build()
+                )
+            ).build()
+        )
+        .build()
+
+val lsTool = Tool.builder().functionDeclarations(mutableListOf(lsDeclaration)).build()
+
 
 fun main() = runBlocking {
     val apiKey = System.getenv("GEMINI_API_KEY")
     val modelId = "gemini-2.0-flash"
-    val client = Client.builder().apiKey(apiKey).httpOptions(HttpOptions.builder().apiVersion("v1").build()).build()
-//    val chat = client.chats.create()
+    val client = Client.builder().apiKey(apiKey).httpOptions(HttpOptions.builder().apiVersion("v1beta").build()).build()
     val systemPrompt = """
         You are a chat bot. My questions will start with ">" and your answers start with "<".
     """.trimIndent()
@@ -19,9 +37,28 @@ fun main() = runBlocking {
             break
         }
         history = history + "\n" + userInput
-        val response = client.models.generateContent(modelId, history, null)
-        val text = response.text()
-        history = history + "\n" + text
-        println(text)
+        val config = GenerateContentConfig.builder().tools(mutableListOf(lsTool)).build()
+        val response = client.models.generateContent(modelId, history, config)
+        if (response.functionCalls() != null && response.functionCalls()!!.isNotEmpty()) {
+            val f = response.functionCalls()!!.first()
+            val path = f.args().get().get("path") as String
+            val lsResult = Runtime.getRuntime().exec("ls $path")
+            val lsOutput = lsResult.inputStream.bufferedReader().readText()
+
+            val part = Part.builder().functionResponse(
+                FunctionResponse.builder().name("ls").response(
+                    mapOf("output" to lsOutput)
+                ).build()
+            ).build()
+            val response =
+                client.models.generateContent(modelId, Content.builder().parts(mutableListOf(part)).build(), config)
+            val text = response.text()
+            history = history + "\n" + text
+            println(text)
+        } else {
+            val text = response.text()
+            history = history + "\n" + text
+            println(text)
+        }
     }
 }

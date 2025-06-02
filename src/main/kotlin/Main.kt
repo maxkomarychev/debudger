cb8383092a64b78e31342ee112435e7a3242d715
Max Komarychev <maxkomarychev@gmail.com>
2025-06-02 11:09:09 +0200

    Better loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ package com.aiagent
 
 import com.google.genai.Client
 import com.google.genai.types.*
-import kotlinx.coroutines.runBlocking
 
 val shellCommandDeclaration = FunctionDeclaration.builder().name("shell_command")
     .description("Execute arbitrary command in shell and get response back").parameters(
@@ -22,47 +21,63 @@ val shellCommandDeclaration = FunctionDeclaration.builder().name("shell_command"
 val shellCommandTool = Tool.builder().functionDeclarations(mutableListOf(shellCommandDeclaration)).build()
 
 
-fun main() = runBlocking {
+fun main() {
     val apiKey = System.getenv("GEMINI_API_KEY")
 //    val modelId = "gemini-2.0-flash"
 //    val modelId = "gemini-2.5-flash-preview-05-20"
     val modelId = "gemini-2.5-pro-preview-05-06"
     val client = Client.builder().apiKey(apiKey).httpOptions(HttpOptions.builder().apiVersion("v1beta").build()).build()
     val currentDir = System.getProperty("user.dir")
-    val systemPrompt = """
-        You are a chat bot.
-        Questions of a user wrapped in <user> and answers wrapped in <bot>.
-        Function calls look like:
-          <function name="xxx" args={...}>
-            <stdout></stdout>
-            <stderr></stderr>
-            <exitCode></exitCode>
-          </function>
+    val firstPrompt = """
+        You are an agent which helps engineers solve technical problems.
+        You may be asked to:
+            - do a research
+            - fix a bug
+            - edit code
+            - etc
+
         If user is asking to do something first consider using shell_command with any of known CLI tools.
+        
         Your current directory is $currentDir.
+        
         When answering a user read entire history of the conversation.
+        
         Do not ask permission to use tools, just use them.
+        
+        
+        Good luck!
     """.trimIndent()
-    var history = "" //systemPrompt
+    val first = Content.builder().role("user").parts(listOf(Part.builder().text(firstPrompt).build())).build()
+    val history = mutableListOf(first)
+    val tools = listOf(
+        shellCommandTool
+    )
+
+    val pendingPrompts = mutableListOf<Content>()
+    val config = GenerateContentConfig.builder().tools(tools).build()
+
     while (true) {
-        val userInput = readLine()
-        if (userInput == "exit") {
-            break
+
+        if (pendingPrompts.isEmpty()) {
+            val userInput = readLine()
+            if (userInput == "exit") {
+                break
+            }
+            val content = Content.builder().role("user").parts(listOf(Part.builder().text(userInput).build())).build()
+            pendingPrompts.add(content)
         }
-        history = """
-            $history
-            <user>$userInput</user>"
-        """.trimIndent()
-        val tools = listOf(
-            shellCommandTool
-        )
-        val config = GenerateContentConfig.builder().tools(tools).build()
+
+        val currentPrompt = pendingPrompts.removeFirst()
+        history.add(currentPrompt)
         val response = client.models.generateContent(modelId, history, config)
+
+
         if (response.functionCalls() != null && response.functionCalls()!!.isNotEmpty()) {
-            val f = response.functionCalls()!!.first()
-            when (f.name().get()) {
+            val functionCall = response.functionCalls()!!.first()
+            val functionName = functionCall.name().get()
+            when (functionName) {
                 "shell_command" -> {
-                    val command = f.args().get().get("command") as String
+                    val command = functionCall.args().get().get("command") as String
                     println("!!! I want to execute a shell command. Do you confirm? $command")
                     val response = readLine()
                     if (response != "yes") {
@@ -70,7 +85,6 @@ fun main() = runBlocking {
                             modelId, "I do not allow running this command", config
                         )
                         val text = response.text()
-                        history = "$history\n<bot>$userInput</bot>"
                         println(text)
                     } else {
                         val process = Runtime.getRuntime().exec(command)
@@ -78,7 +92,7 @@ fun main() = runBlocking {
                         val stdout = process.inputStream.bufferedReader().readText()
                         val stderr = process.errorStream.bufferedReader().readText()
                         val partFunctionResponse = Part.builder().functionResponse(
-                            FunctionResponse.builder().name(f.name().get()).response(
+                            FunctionResponse.builder().name(functionName).response(
                                 mapOf(
                                     "exitCode" to exitCode,
                                     "stdout" to stdout,
@@ -86,42 +100,14 @@ fun main() = runBlocking {
                                 )
                             ).build()
                         ).build()
-                        val partFunctionCall = Part.builder().functionCall(f).build()
-                        history = """
-                            $history
-                            <function name="shell_command" command="$command">
-                                <stdout>$stdout</stdout>
-                                <stderr>$stderr</stderr>
-                                <exitCode>$exitCode</exitCode>
-                            </function>
-                        """.trimIndent()
-                        val historyPart =
-                            Content.builder().role("user").parts(listOf(Part.builder().text(history).build())).build()
-                        val contentFunctionCall =
-                            Content.builder().role("model").parts(listOf(partFunctionCall)).build()
                         val contentFunctionResponse =
                             Content.builder().role("user").parts(listOf(partFunctionResponse)).build()
-                        val response = client.models.generateContent(
-                            modelId,
-                            listOf(historyPart, contentFunctionCall, contentFunctionResponse),
-                            config
-                        )
-                        val text = response.text()
-                        history = """
-                             "$history
-                             <bot>$userInput</bot>"
-                        """.trimIndent()
-                        println(text)
+                        pendingPrompts.add(contentFunctionResponse)
                     }
                 }
             }
-        } else {
-            val text = response.text()
-            history = """
-                $history
-                <bot>$userInput</bot>"
-                """.trimIndent()
-            println(text)
+        } else if (response.text() != null) {
+            println("< ${response.text()}")
         }
     }
 }
